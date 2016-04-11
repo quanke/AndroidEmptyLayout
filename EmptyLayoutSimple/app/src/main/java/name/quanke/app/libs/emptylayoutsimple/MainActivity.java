1cb0e08e17c9488d76b93b684a1ca22ac2c9799f
quanke <609319858@qq.com>
2016-04-11 19:23:08 +0800

    init project

---
+++ EmptyLayoutSimple/app/src/main/java/name/quanke/app/libs/emptylayoutsimple/MainActivity.java
@@ -0,0 +1,90 @@
+package name.quanke.app.libs.emptylayoutsimple;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import name.quanke.app.libs.emptylayout.EmptyLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textHello;
+    private EmptyLayout emptyLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textHello = (TextView) findViewById(R.id.textHello);
+
+        emptyLayout = (EmptyLayout) findViewById(R.id.emptyLayout);
+
+        findViewById(R.id.btnLoading).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emptyLayout.showLoading();
+            }
+        });
+        findViewById(R.id.btnEmpty).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emptyLayout.showEmpty();
+            }
+        });
+        findViewById(R.id.btnError).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emptyLayout.showError();
+            }
+        });
+        findViewById(R.id.btnData).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emptyLayout.hide();
+            }
+        });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
